a5b5415dd095b8aab6b8f8a7adff29473afdfc1a
Stephen Donner <stephen.donner@gmail.com>
2017-05-09 09:54:57 -0700

    Beginnings of Docker support

--- zap-active-api-scan.py
+++ zap-active-api-scan.py
@@ -2,11 +2,19 @@
 
 import argparse
 import datetime
+import json
+import logging
+import os
+import os.path
+import socket
+import subprocess
 import time
+import traceback
 from pprint import pprint
 from zapv2 import ZAPv2
 
 
+running_in_docker = os.path.exists('/.dockerenv')
 time_format = "%a %b %d %H:%M%S %Y"
 
 # Change to match the API key set in ZAP, or use None if the API key is disabled
@@ -23,6 +31,40 @@ parser.add_argument('-t', action='store', dest='target', help='Target URL')
 parser.add_argument('-o', action='store', dest='openapi_url', help='OpenAPI definition URL')
 args = parser.parse_args()
 
+def main(argv):
+
+    base_dir = ''
+    port = 0
+    report_html = ''
+    zap_ip = 'localhost'
+
+if running_in_docker:
+    base_dir = '/zap/wrk/'
+
+if port == 0:
+    while True:
+        port = randit(32768, 61000)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if not (sock.connect_ex(('127.0.0.1', port)) == 0):
+            break
+
+logging.debug('Using port: ') + str(port))
+
+if running_in_docker:
+    try:
+      logging.debug ('Starting ZAP')
+      params = ['zap-x.sh', '-daemon',
+                '-port', str(port),
+                '-host', '0.0.0.0',
+                '-config', 'api.disablekey=true',
+                '-config', 'api.addrs.addr.name=.*',
+                '-config', 'api.addrs.addr.regex=true',
+                '-config', 'spider.maxDuration=' + str(mins),
+                '-addonupdate',
+                '-addoninstall', 'openapi']	# In case we're running in the stable container
+
+      with open('zap.out', "w") as outfile:
+          subprocess.Popen(params, stdout=outfile)
 zap.core.new_session()
 
 zap._request(zap.base + 'openapi/action/importUrl/', {'url': args.openapi_url})
